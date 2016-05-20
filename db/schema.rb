a38d6a08093cb95fb09b86159ac551172e98911f
Garrett <garrettgsb@gmail.com>
2016-05-20 23:42:54 +0000

    Updated seed data; Have not touched Schedule yet

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160520210218) do
+ActiveRecord::Schema.define(version: 20160520230243) do
 
   create_table "encounters", force: :cascade do |t|
     t.string   "defender_id"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160520210218) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "location"
+    t.string   "img_url"
   end
 
   create_table "users", force: :cascade do |t|
